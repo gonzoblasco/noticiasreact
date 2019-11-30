0b99ff239390fedbf3bacfe31fe7a0d697293cd0
gonzoblasco <54Gb2406>
2019-11-30 16:16:24 -0300

    Antes de los PropTypes

---
+++ src/components/Formulario.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+class Formulario extends Component {
+  state = { categoria: "" };
+
+  componentDidMount() {
+    this.setState({
+      categoria: "general"
+    });
+  }
+
+  handleChange = e => {
+    this.setState({
+      categoria: e.target.value
+    }, () => {
+      this.props.consultarNoticias(this.state.categoria);
+    });
+  };
+
+  render() {
+    return (
+      <div className="buscador row">
+        <div className="col s12 m8 offset-m2">
+          <form>
+            <h2>Encuentra Noticias por Categoría</h2>
+
+            <div className="input-field s12 m8 offset-m2">
+              <select onChange={ this.handleChange }>
+                <option value="general">General</option>
+                <option value="science">Ciencia</option>
+                <option value="sports">Deportes</option>
+                <option value="entertainment">Entretenimiento</option>
+                <option value="business">Negocios</option>
+                <option value="health">Salud</option>
+                <option value="technology">Tecnologia</option>
+              </select>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Formulario;
